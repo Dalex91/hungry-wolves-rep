5b960615c93d3eecf356699182d27da3b0b1c3ac
Dalex <bogdandig91@gmail.com>
2022-08-03 18:20:57 +0300

    Update home screen

--- app/src/main/java/com/example/hungrywolves/MainActivity.kt
+++ app/src/main/java/com/example/hungrywolves/MainActivity.kt
@@ -1,15 +1,12 @@
 package com.example.hungrywolves
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.view.WindowManager
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_home_screen)
+        setContentView(R.layout.activity_main)
+
     }
 }
