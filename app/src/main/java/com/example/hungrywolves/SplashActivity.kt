b71bd18fb33f4fb37b40e9f366ade0983b895e2c
Dalex <bogdandig91@gmail.com>
2022-08-08 12:50:19 +0300

    Homescreen done

--- app/src/main/java/com/example/hungrywolves/SplashActivity.kt
+++ app/src/main/java/com/example/hungrywolves/SplashActivity.kt
@@ -3,11 +3,14 @@ package com.example.hungrywolves
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
 import android.view.WindowManager
+import androidx.activity.viewModels
+import com.example.hungrywolves.model.SplashScreenViewModel
 
-const val TIME_FOR_LOADING : Long = 3000
+
 class SplashActivity : AppCompatActivity() {
+    private val viewModelSplashScreen : SplashScreenViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
@@ -17,10 +20,7 @@ class SplashActivity : AppCompatActivity() {
         )
         
         supportActionBar?.hide()
-        Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()
-        }, TIME_FOR_LOADING)
+        viewModelSplashScreen.delayIntro(Intent(this, MainActivity::class.java),
+            this)
     }
 }
