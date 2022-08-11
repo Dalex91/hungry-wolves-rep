d3dba208ac172ea3abe63edfdbdda748ab78d049
Dalex <bogdandig91@gmail.com>
2022-08-11 14:12:50 +0300

    Get started

--- app/src/main/java/com/example/hungrywolves/network/Meal.kt
+++ app/src/main/java/com/example/hungrywolves/network/Meal.kt
@@ -5,5 +5,5 @@ import com.squareup.moshi.Json
 data class Meal(
     val idMeal: Int,
     @Json(name = "strMeal") val name: String,
-    @Json(name = "strMealThumb") val mealUrl : String,
+    @Json(name = "strMealThumb") val mealUrl : String
 )
