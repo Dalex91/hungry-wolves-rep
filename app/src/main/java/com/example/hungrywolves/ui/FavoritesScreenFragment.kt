741a72674b54154ecad4c6d8a4b4caae96a72d17
Dalex <bogdandig91@gmail.com>
2022-08-18 09:41:38 +0300

    Get started

--- app/src/main/java/com/example/hungrywolves/ui/FavoritesScreenFragment.kt
+++ app/src/main/java/com/example/hungrywolves/ui/FavoritesScreenFragment.kt
@@ -6,16 +6,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.hungrywolves.adapters.FavouriteAdapter
 import com.example.hungrywolves.databinding.FragmentFavoritesScreenBinding
+import com.example.hungrywolves.model.FavouritesScreenViewModel
 
 class FavoritesScreenFragment : Fragment() {
+    private val viewModelFavScreen : FavouritesScreenViewModel by viewModels()
     private lateinit var binding : FragmentFavoritesScreenBinding
+    private val favAdapter by lazy {
+        FavouriteAdapter()
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentFavoritesScreenBinding.inflate(inflater)
+        binding.apply {
+            lifecycleOwner = this@FavoritesScreenFragment
+            favouritesReycleView.adapter = favAdapter
+            viewModel = viewModelFavScreen
+        }
         return binding.root
     }
 }
