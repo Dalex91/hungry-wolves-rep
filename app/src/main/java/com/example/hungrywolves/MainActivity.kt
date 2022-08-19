3c501a205815cc94c20fcd268e6fe50b94cf7387
Dalex <bogdandig91@gmail.com>
2022-08-19 03:52:02 +0300

    Some progress

--- app/src/main/java/com/example/hungrywolves/MainActivity.kt
+++ app/src/main/java/com/example/hungrywolves/MainActivity.kt
@@ -2,17 +2,26 @@ package com.example.hungrywolves
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.fragment.app.FragmentContainer
+import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
+import androidx.navigation.fragment.FragmentNavigator
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.hungrywolves.databinding.ActivityMainBinding
+import com.example.hungrywolves.network.ConnectionLiveData
+import com.google.firebase.firestore.remote.ConnectivityMonitor
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var connectionLiveData: ConnectionLiveData
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +29,7 @@ class MainActivity : AppCompatActivity() {
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
         supportActionBar?.hide()
+
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -27,6 +37,7 @@ class MainActivity : AppCompatActivity() {
             R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
 
+        //initConnection()
         navController.addOnDestinationChangedListener{_, destination, _ ->
             binding.menuNavigation.apply {
                 visibility = when(destination.id) {
@@ -39,4 +50,11 @@ class MainActivity : AppCompatActivity() {
         binding.menuNavigation.setupWithNavController(navController)
     }
 
+    fun initConnection() {
+        connectionLiveData = ConnectionLiveData(this)
+        connectionLiveData.observe(this) {isNetworkAvailable ->
+            if(isNetworkAvailable == false)
+                navController.navigate(R.id.no_internet_screen_fragment)
+        }
+    }
 }
